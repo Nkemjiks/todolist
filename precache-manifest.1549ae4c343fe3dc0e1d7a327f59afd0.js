85e0bf91aa4b599b6527030cef0766dca3346f71
nkemjiks <mbonunkemjika@ymail.com>
2019-01-22 23:16:00 +0100

    Updates

---
+++ precache-manifest.1549ae4c343fe3dc0e1d7a327f59afd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b566cca4284ccc5e3acfc330558b809",
+    "url": "/todolist/static/media/logo.1b566cca.png"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "037c357fbf4ea6f3a528",
+    "url": "/todolist/static/js/main.037c357f.chunk.js"
+  },
+  {
+    "revision": "815297a0692c18394c82",
+    "url": "/todolist/static/js/1.815297a0.chunk.js"
+  },
+  {
+    "revision": "037c357fbf4ea6f3a528",
+    "url": "/todolist/static/css/main.401ce640.chunk.css"
+  },
+  {
+    "revision": "d836ec453425e399229f53f114b941ae",
+    "url": "/todolist/index.html"
+  }
+];
